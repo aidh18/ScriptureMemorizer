b060664e78dda2b45b4113ad94835dd7285a0fcd
Aidan Stacey <98003687+aidh18@users.noreply.github.com>
2023-02-21 12:16:32 -0700

    Uploaded

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ScriptureMemorizer
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book();
+
+            Console.WriteLine("Welcome to the Scripture Memorizer\n");
+            Console.WriteLine("Choose a random verse (1) or use your own (2)? ");
+            string input = Console.ReadLine();
+
+            if (input == "1")
+            {
+                book.ChooseScripture();
+            }
+            else
+            {
+                book.InputNewScripture();
+            }
+
+            Scripture scripture = new Scripture(book.GetScripture());
+
+            scripture.DisplayScripture();
+            Console.WriteLine("Press enter to hide words, or 'quit' to exit.");
+            input = Console.ReadLine();
+            while (input != "quit")
+            {
+                Console.Clear();
+                scripture.HideWords();
+                scripture.DisplayScripture();
+
+                Console.WriteLine("Press enter to hide words, or 'quit' to exit.");
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
